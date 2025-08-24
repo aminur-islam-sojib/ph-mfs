4781a8ea4f55f44e4002d29aad2a0cb0c7b9f572
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-08-24 10:52:06 +0600

    add form validation

--- src/assets/components/LoginForm/LoginForm.jsx
+++ src/assets/components/LoginForm/LoginForm.jsx
@@ -1,18 +1,37 @@
 import bg from '../../../assets/bg.png';
 import group from '../../../assets/Group.png';
 import { useNavigate } from 'react-router-dom';
+import '../../../index.css';
+import { useState } from 'react';
 
 const LoginForm = () => {
+  const userDetails = [
+    { number: 1757829428, pin: 1234 },
+    { number: 1757829429, pin: 1234 },
+  ];
+
   const navigate = useNavigate();
+  const [phone, setNumber] = useState();
+  const [pass, setPin] = useState();
+  const [err, setErr] = useState(false);
 
   const handleCLick = () => {
-    navigate('/home');
+    const user = userDetails.find(
+      (user) => Number(phone) === user.number && Number(pass) === user.pin
+    );
+
+    if (user) {
+      navigate('/home');
+    } else {
+      setErr('Invalid Credentials');
+      // alert('Invalid Credentials');
+    }
   };
 
   return (
     <>
       <div className=" h-screen flex items-center">
-        <div className=" mx-auto w-[300px] h-[500px] border border-gray-300 rounded-lg shadow-lg p-4 flex flex-col justify-between">
+        <div className=" mx-auto w-[300px] h-[500px] border border-gray-300 rounded-lg shadow-lg p-4 flex flex-col justify-between relative">
           <div className=" flex flex-col items-center justify-center relative">
             <img src={bg} alt="" height={50} />
             <img src={group} alt="" height={50} className=" absolute top-7" />
@@ -22,23 +41,26 @@ const LoginForm = () => {
               <p className=" text-gray-500 text-sm">Login to your account</p>
             </div>
 
-            <div className=" gap-2 flex flex-col mb-16 w-full">
-              <div>
-                <label className=" font-medium">Mobile Number</label>
+            <div className=" loginForm gap-2 flex flex-col mb-16 w-full absolute">
+              <div className=" flex flex-col gap-1.5">
+                <label className=" font-medium ">Mobile Number</label>
                 <input
                   type="text"
                   placeholder="Enter Your Phone Number"
-                  className="input"
+                  className="input  focus-visible:outline-0"
+                  onChange={(e) => setNumber(e.target.value)}
                 />
               </div>
-              <div>
+              <div className=" flex flex-col gap-1.5">
                 <label className=" font-medium">Pin</label>
                 <input
                   type="number"
                   placeholder="Enter Your Phone Number"
-                  className="input"
+                  className="input focus-visible:outline-0 "
+                  onChange={(e) => setPin(e.target.value)}
                 />
               </div>
+              {err && <p className=" text-red-500 text-sm">{err}</p>}
               <button
                 onClick={handleCLick}
                 className="btn btn-active btn-primary mt-2 active:scale-95 hover:bg-blue-800"
